8e713091d4df87c1d51cfa691f76082314620727
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 02:34:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(customer)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-puts line = katz_deli << customer
+puts katz_deli = katz_deli << customer
   end
 end
 
