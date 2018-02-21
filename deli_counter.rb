bd357724642041642b9feb2f798345426294a6ad
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 03:15:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-katz_deli.each {|person| puts "The line is currently: #{katz_deli.index} + #{person}"}
+katz_deli.each {|person| puts "The line is currently: #{katz_deli.index + 1} + #{person}"}
   end
 end
 
