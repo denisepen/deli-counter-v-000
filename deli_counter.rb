8304644bbef3915d38f4b3730c48dcb795744cd5
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 03:03:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,12 +20,12 @@ end
     def now_serving(katz_deli)
       if katz_deli.length == 0
         puts "There is nobody waiting to be served!"
-      else katz_deli.each do
+      else 
         # next_customer = katz_deli.unshift
         puts "Currently serving #{katz_deli.shift}."
       end
     end
-  end
+  
 
 
 
