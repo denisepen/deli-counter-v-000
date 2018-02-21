c6f60e0c77d1e711f74f3d88c8c4c6c84c310569
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 02:26:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+katz_deli = []
+
+def line
+  # line method shows everyone their current place in line
+  if katz_deli.length == 0 
+    "The line is currently empty."
+  else 
+    XXXXX
+end
+
+    def take_a_number(katz_deli, person)
+
+      
+
+    end
+
+ 
+
+# function takeANumber(katzDeliLine, newPerson) {
+#   katzDeliLine.push(newPerson);
+#   return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
+# }
